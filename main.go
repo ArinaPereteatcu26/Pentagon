bac90fb66ff8ce41825326de250c740690dec9bc
Nejintev Nicolai <niksaysit@gmail.com>
2023-09-21 12:41:29 +0300

    Add DB capabilities

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gin-gonic/gin"
+	"net/url"
+	"os"
+)
 
 func main() {
+
+	u := &url.URL{
+		Scheme: "sqlserver",
+		User:   url.UserPassword(os.Getenv("DB_LOGIN"), os.Getenv("DB_PASSWORD")),
+		Host:   fmt.Sprintf("%s:%d", os.Getenv("DB_HOST"), 1433),
+	}
+
+	_, err := sql.Open("sqlserver", u.String())
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	// listen and serve on 0.0.0.0:80
-	err := r.Run(":8080")
+	// listen and serve on 0.0.0.0:8080
+	err = r.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
