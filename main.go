9d7d8fda08af8188787aa8f4bb5efc4ab6fe1719
Nejintev Nicolai <niksaysit@gmail.com>
2023-09-21 12:12:22 +0300

    Create basic server

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package Pentagon
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// listen and serve on 0.0.0.0:80
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
