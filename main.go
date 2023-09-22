36d800964eb27e7a3ba0ab4acc88c970025e7299
Berco Andrei <andrei2003berco@gmail.com>
2023-09-22 10:05:04 +0300

    changed the structure and added errors handlers

--- main.go
+++ main.go
@@ -2,41 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
 	"net/url"
+	"os"
 )
 
 var db *sql.DB
 
 type Hotspot struct {
-	HotspotID string `json:"HotspotID"`
-	Title     string `json:"Title"`
-	Latitude  string `json:"Latitude"`
-	Longitude string `json:"Longitude"`
+	HotspotID int     `json:"hotspot-id"`
+	Title     string  `json:"title"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
 
-func hotspotsJson() []Hotspot {
+func hotspotsJson() ([]Hotspot, error) {
 	var hotspots []Hotspot
-	u := &url.URL{
-		Scheme: "sqlserver",
-		User:   url.UserPassword("sa", "Skies-Backer-Overlord1-Voucher"),
-		Host:   fmt.Sprintf("%s:%d", "49.13.85.200", 1433),
-	}
-	db, err := sql.Open("sqlserver", u.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 	rows, err := db.Query("USE PhotoApp SELECT HotspotsID, Title, Latitude, Longitude FROM Hotspots")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(rows)
 	for rows.Next() {
 		var hotspot Hotspot
 
@@ -47,49 +41,45 @@ func hotspotsJson() []Hotspot {
 			&hotspot.Longitude,
 		)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		hotspots = append(hotspots, hotspot)
 	}
-	//var hotspotsJson, _ = json.MarshalIndent(hotspots, "", "  ") // Indent with two spaces
-	return hotspots
+	return hotspots, nil
 }
 
 func main() {
-	fmt.Println("-----------------------------------------------------------")
-
-	//extracting from DB the values
-	hotspots := hotspotsJson()
-
-	//Here is created a map with keys(title of hotspot) and values(coordinates of hotspot)
-	//Don't know if we will use it but let it be for the moment
-	var hotspotsMap = make(map[string]string)
-	j := len(hotspots)
-	for i := 0; i < j; i++ {
-		hotspotsMap[hotspots[i].Title] = hotspots[i].Latitude + "," + hotspots[i].Longitude
+	u := &url.URL{
+		Scheme: "sqlserver",
+		User:   url.UserPassword(os.Getenv("DB_LOGIN"), os.Getenv("DB_PASS")),
+		Host:   fmt.Sprintf("%s:%d", os.Getenv("DB_SERVER"), 1433),
 	}
-	for key := range hotspotsMap {
-		fmt.Printf(" %s,  %s\n",
-			key, hotspotsMap[key])
+	var err error
+	db, err = sql.Open("sqlserver", u.String())
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
 
 	//http://localhost:8080/hotspots
+	//extracting from DB the values
 	//Check if this is OK
 	r := gin.Default()
 	r.GET("/hotspots", func(c *gin.Context) {
-		// Marshal the hotspots data with indentation
-		hotspotsJSON, err := json.MarshalIndent(hotspots, "", "  ")
+		hotspots, err := hotspotsJson()
 		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
+			log.Fatal(err)
 		}
-		c.Header("Content-Type", "application/json")
-		c.String(http.StatusOK, string(hotspotsJSON))
+		c.JSON(200, hotspots)
 	})
-	//listen and serve on 0.0.0.0:8080
-	var err = r.Run(":8080")
+	//listen and serve on localhost:8080
+	err = r.Run(":8080")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 }
